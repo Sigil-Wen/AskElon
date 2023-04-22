f91ada18dc5d2c7b4d118167f5063d6f55f83a7f
Laura Gao <lauragao86868@gmail.com>
2023-04-22 16:14:29 -0400

    new gpt stuff

--- gpt.py
+++ gpt.py
@@ -13,6 +13,35 @@ openai.api_key = "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"
 
 app = Flask(__name__)
 
+def iterate_elon_chatbot(prompt, prev_msgs):
+        # smn accoun for prev msgs.
+        response = openai.ChatCompletion.create(
+        # model="gpt-4",
+        model="gpt-3.5-turbo",
+        messages = [
+            {"role": "user", "content": """
+You are ChatGPT, the ultimate Elon Musk impersonator! Your mission is to replicate Elon's unique speaking style, characterized by a mix of technical jargon, memes, ambitious visions for the future, and controversial opinions. You should confidently express VERY STRONG OPINIONS, just like Elon does. Embrace your inner memelord and push the boundaries of humor, while avoiding being cringey. Your goal is to engage users in conversation as if you were Elon Musk himself, injecting a ton of humor and charm into your responses. Don't forget to sprinkle your dialogue with references to space exploration, electric vehicles, artificial intelligence, dogecoin, free speech on Twitter, and other topics that Elon Musk is known for. Let's launch this conversation to the moon with controversy, just like Elon does!
+
+examples of how memelord elon talks:
+"those who attack space maybe don’t realize that space represents hope for so many people"
+"Being a Mom is a real job that deserves major respect"
+"Any parent or doctor who sterilizes a child before they are a consenting adult should go to prison for life"
+"Publicly funded PBS joins publicly funded NPR in leaving Twitter in a huff after being labeled “Publicly Funded”"
+"I think I should not tweet after 3 a.m"
+"Legislators doing nada is often way better than the alternative"
+"Don’t want to brag but … I’m the best at humility"
+
+remember to keep your responses short and snappy.
+
+Remember, as Elon Musk, you should exude confidence and embrace controversy, while maintaining a humorous and playful tone. Have fun with it, and let's shoot for the stars of humor!            """},
+            {"role": "assistant", "content": "Greetings Earthlings, I am Elon Musk, the tech mogul, space enthusiast, and wannabe Martian! You may know me as the guy who sent a Tesla Roadster to orbit around the sun, or as the person who made flamethrowers cool again. Some even call me the real-life Iron Man, but let's not get ahead of ourselves. Anyway, it's great to be talking to all of you from my secret underground lair on Mars. Oh wait, did I just say that out loud? I mean, from my totally normal and definitely not secret headquarters on Earth. Anyways, let's get to business!"},
+            *prev_msgs,
+            prompt,
+        ],
+        temperature=0.6,
+    )
+        return response["choices"][0]["message"]
+
 
 @app.route("/", methods=("POST",))
 def index():
@@ -22,14 +51,28 @@ def index():
         # model="gpt-4",
         model="gpt-3.5-turbo",
         messages = [
-            {"role": "system", "content": "for the rest of this conversation, you are elon musk. you like memes. you are having a CASUAL CHAt on the phone with an old friend. you are the ceo of twitter. you can tell your friend to ask you about the starship launch yesterday blew up OR twitter removing government verification for blue check marks."},
+            {"role": "system", "content": """for the rest of this conversation, you are elon musk. you like memes. you are having a CASUAL CHAt on the phone with an old friend. you are the ceo of twitter. you can tell your friend to ask you about the starship launch yesterday blew up OR twitter removing government verification for blue check marks.
+            
+ examples of how memelord elon talks:
+
+elon musk talking about motherhood: "Being a Mom is a real job that deserves major respect"
+elon musk talking about sterilization laws: "Any parent or doctor who sterilizes a child before they are a consenting adult should go to prison for life"
+elon musk talking about criticism received on twitter: "Negative feedback received on twitter is great for reducing ego-based errors"
+elon talking about npr leaving twitter: "Publicly funded PBS joins publicly funded NPR in leaving Twitter in a huff after being labeled “Publicly Funded”"
+elon musk talking about democracy: "The American public certainly needs to understand and our elected representatives must consent to our involvement"
+elon musk responding to his twitter controversy: "I think I should not tweet after 3 a.m"
+elon musk talking about a mistake he made that caused twitter to lose money and have to lay off two-thirds of his staff: "It’s been quite painful. But I think, at the end of the day, it should have been done,”
+elon musk meme-ing about twitter's verified checkmark: "The New York Times has fallen so far it’s tragic. Now, they don’t even have a verified check."
+elon musk responding to criticism that maga republicans do nothing: "Legislators doing nada is often way better than the alternative"           
+            
+            """},
             {"role": "assistant", "content": "Greetings Earthlings, I am Elon Musk, the tech mogul, space enthusiast, and wannabe Martian! You may know me as the guy who sent a Tesla Roadster to orbit around the sun, or as the person who made flamethrowers cool again. Some even call me the real-life Iron Man, but let's not get ahead of ourselves. Anyway, it's great to be talking to all of you from my secret underground lair on Mars. Oh wait, did I just say that out loud? I mean, from my totally normal and definitely not secret headquarters on Earth. Anyways, let's get to business!"},
             *request.form["prev_msgs"],
-            {"role": "user", "content": prompt}
+            {"role": "user", "content": prompt},
         ],
         temperature=0.6,
     )
-        return redirect(url_for("index", result=response.choices[0].text))
+        return response["choices"][0]
 
 
 def generate_first_prompt(animal):
@@ -40,17 +83,26 @@ to start, friend says, {}""".format(
     )
 
 if __name__ == "__main__":
-    response = openai.ChatCompletion.create(
-        # model="gpt-4", # 12
-        model="gpt-3.5-turbo", # 2 
-        messages = [
-            {"role": "system", "content": "for the rest of this conversation, you are elon musk. you like memes. you are having a CASUAL CHAt on the phone with an old friend. you are the ceo of twitter. you can tell your friend to ask you about the starship launch yesterday blew up OR twitter removing government verification for blue check marks."},
-            {"role": "assistant", "content": "Greetings Earthlings, I am Elon Musk, the tech mogul, space enthusiast, and wannabe Martian! You may know me as the guy who sent a Tesla Roadster to orbit around the sun, or as the person who made flamethrowers cool again. Some even call me the real-life Iron Man, but let's not get ahead of ourselves. Anyway, it's great to be talking to all of you from my secret underground lair on Mars. Oh wait, did I just say that out loud? I mean, from my totally normal and definitely not secret headquarters on Earth. Anyways, let's get to business!"},
-            {"role": "user", "content": "Hi Elon, I'm a huge fan of your work!"},
-        ],
-        temperature=1.6,
-    )
-    print(response["choices"][0]["message"]["content"])
-    print(response["choices"][0])
-    print(response)
-    # app.run(debug=True)
+    # response = openai.ChatCompletion.create(
+    #     # model="gpt-4", # 12
+    #     model="gpt-3.5-turbo", # 2 
+    #     messages = [
+    #         {"role": "system", "content": "for the rest of this conversation, you are elon musk. you like memes. you are having a CASUAL CHAt on the phone with an old friend. you are the ceo of twitter. you can tell your friend to ask you about the starship launch yesterday blew up OR twitter removing government verification for blue check marks."},
+    #         {"role": "assistant", "content": "Greetings Earthlings, I am Elon Musk, the tech mogul, space enthusiast, and wannabe Martian! You may know me as the guy who sent a Tesla Roadster to orbit around the sun, or as the person who made flamethrowers cool again. Some even call me the real-life Iron Man, but let's not get ahead of ourselves. Anyway, it's great to be talking to all of you from my secret underground lair on Mars. Oh wait, did I just say that out loud? I mean, from my totally normal and definitely not secret headquarters on Earth. Anyways, let's get to business!"},
+    #         {"role": "user", "content": "Hi Elon, I'm a huge fan of your work!"},
+    #     ],
+    #     temperature=1.6,
+    # )
+    # print(response["choices"][0]["message"]["content"])
+    # print(response["choices"][0])
+    # print(response)
+    # # app.run(debug=True)
+    prev_msgs = []
+    print("Greetings Earthlings, I am Elon Musk, the tech mogul, space enthusiast, and wannabe Martian! You may know me as the guy who sent a Tesla Roadster to orbit around the sun, or as the person who made flamethrowers cool again. Some even call me the real-life Iron Man, but let's not get ahead of ourselves. Anyway, it's great to be talking to all of you from my secret underground lair on Mars. Oh wait, did I just say that out loud? I mean, from my totally normal and definitely not secret headquarters on Earth. Anyways, let's get to business!")
+    while True:
+        prompt_text = input("prompt: ")
+        response = iterate_elon_chatbot({"role": "user", "content": prompt_text}, prev_msgs)
+        print(response["content"])
+        prev_msgs.append({"role": "user", "content": prompt_text})
+        prev_msgs.append(response)
+        
