b0aabb0fa2a7e61f9942b92da996bbdd84ce67ef
Sigil Wen <45740645+Sigil-Wen@users.noreply.github.com>
2023-04-22 12:39:02 -0400

    Setting up voice endpoint

--- server.py
+++ server.py
@@ -1,78 +1,19 @@
-import base64
-import json
-import logging
-
 from flask import Flask
-from flask_sockets import Sockets
-from twilio.twiml.voice_response import VoiceResponse, Say
+from twilio.twiml.voice_response import VoiceResponse
 
 app = Flask(__name__)
-sockets = Sockets(app)
-
-HTTP_SERVER_PORT = 5000
-
-# add a get route 
-
-@app.route('/', methods=['GET'])
-def home():
-  return "HELLO WORLD"
-
-@sockets.route('/media')
-def echo(ws):
-    app.logger.info("Connection accepted")
-    # A lot of messages will be sent rapidly. We'll stop showing after the first one.
-    has_seen_media = False
-    message_count = 0
-    while not ws.closed:
-        message = ws.receive()
-        if message is None:
-            app.logger.info("No message received...")
-            continue
-
-        # Messages are a JSON encoded string
-        data = json.loads(message)
-
-        # Using the event type you can determine what type of message you are receiving
-        if data['event'] == "connected":
-            app.logger.info("Connected Message received: {}".format(message))
-        if data['event'] == "start":
-            app.logger.info("Start Message received: {}".format(message))
-        if data['event'] == "media":
-            if not has_seen_media:
-                app.logger.info("Media message: {}".format(message))
-                payload = data['media']['payload']
-                app.logger.info("Payload is: {}".format(payload))
-                chunk = base64.b64decode(payload)
-                app.logger.info("That's {} bytes".format(len(chunk)))
-                app.logger.info("Additional media messages from WebSocket are being suppressed....")
-
-                 # Generate a response using Twilio's VoiceResponse and Say classes
-                resp = VoiceResponse()
-                resp.say("Hello, World!")
-
-                # Send the response back to the caller via the websocket
-                response_data = {
-                    "event": "media",
-                    "media": {
-                        "payload": base64.b64encode(resp.to_xml().encode('utf-8')).decode('ascii')
-                    }
-                }
-                response_message = json.dumps(response_data)
-                ws.send(response_message)
-                has_seen_media = True
-        if data['event'] == "closed":
-            app.logger.info("Closed Message received: {}".format(message))
-            break
-        message_count += 1
-
-    app.logger.info("Connection closed. Received a total of {} messages".format(message_count))
 
 
-if __name__ == '__main__':
-    app.logger.setLevel(logging.DEBUG)
-    from gevent import pywsgi
-    from geventwebsocket.handler import WebSocketHandler
+@app.route("/voice", methods=['GET', 'POST'])
+def voice():
+    """Respond to incoming phone calls with a 'Hello world' message"""
+    # Start our TwiML response
+    resp = VoiceResponse()
 
-    server = pywsgi.WSGIServer(('', HTTP_SERVER_PORT), app, handler_class=WebSocketHandler)
-    print("Server listening on: http://localhost:" + str(HTTP_SERVER_PORT))
-    server.serve_forever()
+    # Read a message aloud to the caller
+    resp.say("hey laura. Hows it going?", voice='alice')
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
